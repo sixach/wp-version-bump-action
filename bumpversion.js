0b171b6f843a804d537064cb3513c93367700aeb
Fred Bradley <hello@fredbradley.co.uk>
2021-03-09 11:52:34 +0000

    initial commit

---
+++ bumpversion.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+
+const core = require('@actions/core')
+const github = require('@actions/github')
+
+const newVersion = core.getInput('version')
+const filePath = core.getInput('file_path')
+
+// Check if File Exists
+if (fs.existsSync(filePath)) {
+    // File Exists
+} else {
+    throw new Error("File " + filePath + " does not exist")
+}
+
+// Valid SemVer
+const regex = new RegExp(/^\d+\.\d+\.\d+$/mg)
+if (regex.test(newVersion)) {
+    console.log("Version Valid")
+} else {
+    console.log("Version Invalid")
+    throw new Error("Version Invalid")
+}
+
+// Now things are valid, lets do the thing...
+// Read the file
+fs.readFile(filePath, 'utf8', function (err, data) {
+    if (err) {
+        // if there's an error of some kind then return that to console log
+        return console.log(err)
+    }
+
+    // change the version string and save to result var
+    var result = data.replace(/^(\s?)+Version: \d+\.\d+\.\d+$/mg, 'Version: ' + newVersion)
+
+    // Write the changes back to the file
+    fs.writeFile(filePath, result, 'utf8', function (err) {
+        if (err) {
+            return console.log(err);
+        }
+    });
+});
+
