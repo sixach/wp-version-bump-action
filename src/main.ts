fd7ba5246bca41ea1035201ccecaf08b7f181c54
r007 <moninsergei@gmail.com>
2022-01-22 03:25:26 +0400

    Add /dist folder, refactor main file to typescript

--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,46 @@
-const fs = require('fs');
+import * as core from '@actions/core'
+import fs from 'fs'
 
-const core = require('@actions/core')
+async function run(): Promise<void> {
+  const newVersion = core.getInput('version')
+  const filePath = core.getInput('file_path')
 
-const newVersion = core.getInput('version')
-const filePath = core.getInput('file_path')
+  // Check if File Exists
+  if (!fs.existsSync(filePath)) {
+    core.setFailed(`⚠️ File ${filePath} does not exist`)
+  }
 
-// Check if File Exists
-if (!fs.existsSync(filePath)) {
-    core.setFailed("File "+ filePath + " does not exist");
-}
+  // Valid SemVer
+  const regex = new RegExp(/^\d+\.\d+\.\d+$/gm)
+  if (regex.test(newVersion)) {
+    core.debug('✅ Version Valid')
+  } else {
+    core.setFailed('❌ Version Invalid')
+  }
 
-// Valid SemVer
-const regex = new RegExp(/^\d+\.\d+\.\d+$/mg)
-if (regex.test(newVersion)) {
-    console.log("Version Valid")
-} else {
-    console.log("Version Invalid")
-    core.setFailed("Version Invalid");
-}
-
-// Now things are valid, lets do the thing...
-// Read the file
-fs.readFile(filePath, 'utf8', function (err, data) {
-    if (err) {
-        // If there's an error of some kind then return that to console log
-        return console.log(err)
+  // Now things are valid, lets do the thing...
+  // Read the file
+  fs.readFile(filePath, 'utf8', function (readError, data) {
+    if (readError instanceof Error) {
+      // If there's an error of some kind then stop execution
+      core.setFailed(`⚠️ Could not read the file - ${readError.message}`)
     }
 
     // Change the version string and save to result var
-    var result = data.replace(/(\s*?Version:\s+?)\d+\.\d+\.\d+/mg, '$1' + newVersion)
+    const result = data.replace(
+      /(\s*?Version:\s+?)\d+\.\d+\.\d+/gm,
+      `$1${newVersion}`
+    )
 
     // Write the changes back to the file
-    fs.writeFile(filePath, result, 'utf8', function (err) {
-        if (err) {
-            return console.log(err);
-        }
-    });
-});
+    fs.writeFile(filePath, result, 'utf8', function (writeError) {
+      if (writeError instanceof Error) {
+        core.setFailed(
+          `⚠️ Could not write the file to disk - ${writeError.message}`
+        )
+      }
+    })
+  })
+}
 
+run()
